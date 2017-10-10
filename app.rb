0da086a3c0353f71c3859e7f7bd1b3f7a913caa0
hugosykes <hugo@denmeadfarm.co.uk>
2017-10-10 14:22:44 +0100

    Extracted ruby functionality into instance variables

--- app.rb
+++ app.rb
@@ -1,11 +1,12 @@
 require 'sinatra'
 get '/' do
-
-erb "Hi there, Visitor <%= 2 + 2 %>!"
-
+  erb 'Hi there, Visitor <%= 2 + 2 %>!'
 end
 
 get '/cat' do
-erb(:index)
-
+  @coder_name = "My name is #{%w[Hugo Pablo].sample}!"
+  @cat_name = "Hey this is a sample from an array  #{%w[Amigo Oscar Viking].sample}"
+  weekday = Time.now.strftime('%A')
+  @weekday = "Today is a #{weekday}!"
+  erb(:index)
 end
