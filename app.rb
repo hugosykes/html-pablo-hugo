d8f9dc88044c0088dc10543f37629c821738d050
Pablo <pabloandresvdl@gmail.com>
2017-10-10 14:38:42 +0100

    add params

--- app.rb
+++ app.rb
@@ -3,8 +3,19 @@ get '/' do
   erb 'Hi there, Visitor <%= 2 + 2 %>!'
 end
 
-get '/cat' do
+get '/random-cat' do
+
   @coder_name = "My name is #{%w[Hugo Pablo].sample}!"
+  @cat_name = "Hey this is a sample from an array  #{%w[Amigo Oscar Viking].sample}"
+  weekday = Time.now.strftime('%A')
+  @weekday = "Today is a #{weekday}!"
+  erb(:index)
+end
+
+get '/named-cat' do
+p  params
+  @coder_name = "#{params[:name]}"
+  #@coder_name = "My name is #{%w[Hugo Pablo].sample}!"
   @cat_name = "Hey this is a sample from an array  #{%w[Amigo Oscar Viking].sample}"
   weekday = Time.now.strftime('%A')
   @weekday = "Today is a #{weekday}!"
